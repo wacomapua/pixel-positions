3538f99595eb87c228b659427744b68980972a9f
Waco <waco@Wacos-MBP.modem>
2024-05-02 18:26:30 +1000

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -9,3 +9,4 @@ export default defineConfig({
         }),
     ],
 });
+// initial commit
