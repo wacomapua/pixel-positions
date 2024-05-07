c9078466d6cebeae361b396be0b61fa37cad533f
Waco Mapua <wacomapua@gmail.com>
2024-05-08 08:14:48 +1000

    Laracasts 30 day Laravel Episode 29

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,18 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="text-xl text-bold group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
-        <p class="mt-4">Full Time - From $60,000</p>
+        <h3 class="text-xl text-bold group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="mt-4">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag size="small">{{ $tag }}</x-tag>
+            @endforeach
         </div>
         <x-employer-logo :width="42"/>
     </div>
